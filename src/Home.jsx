f726c9a3fb27856b390b7a931436ad2e0fb0652c
GeneralTomm <tommysetiawannn@icloud.com>
2024-01-19 09:34:56 +0700

    adding json-server for REST API placeholder

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,44 +2,26 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs , setBlogs] = useState([
-        {
-            id:1,
-            title:"Pernafasan Matahari",
-            author:"Yoriichi Tsugikuni",
-            body:"pernafasan matahari adalah teknik pertama yang ditemukan di era Sengoku oleh seorang pendekar pedang yang bernama Yoriichi tsugikuni"
-        },
-        {
-            id:2,
-            title:"Pernafasan Bulan",
-            author:"Kokushibou",
-            body:"Pernafasan bulan adalah teknik pernafasan yang ditemukan diera sengoku , yang merupakan cabang langsung dari pernafasan matahari."
-        },
-        {
-            id:3,
-            title:"Pernafasan angin",
-            author:"Shinazugawa sanemi",
-            body:"Pernafasan angin merupakan teknik yang ditemukan oleh seorang pendekar pedang dari era Sengoku dan pengguna yang diketahui adalah Shinazugawa Sanemi"
-        }
-    ]);
+    const [blogs , setBlogs] = useState(null);
     
-    const [name,setName] = useState("Kokushibou");
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( i => i.id !== id);
         setBlogs(newBlogs);
     }
+
+
     useEffect(()=> {
-        console.log("useEffect run !");
+        fetch("http://localhost:9000/blogs")
+        .then(res => res.json())
+        .then( res => {
+            console.log(res);
+            setBlogs(res);
+        })
     },[]);
 
-    useEffect(function(){
-        console.log("nilai nama mengalami perubahan");
-    },[name]);
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="Beranda :" handleDelete={handleDelete}/>
-            <button onClick={()=> setName("Michikatsu")}>test</button>
+            {blogs && ( <BlogList blogs={blogs} title="Beranda :" handleDelete={handleDelete}/>)}
         </div>
      );
 }
